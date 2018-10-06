ab922c8b061826c16eceeb7904b94404b20c3de5
David Anderson <matandersonlv@hotmail.com>
2018-10-06 23:45:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,9 +6,6 @@ end
 
 def batch_badge_creator(attendees)
   attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  count = 0
-  while count <= attendees.length-1
-    "Hello, my name is #{attendees}."
-    count += 1
-  end
+  attendees.each |attendee|
+  "Hello, my name is "
 end
